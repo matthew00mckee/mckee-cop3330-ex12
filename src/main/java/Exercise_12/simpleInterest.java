0c4ada45ba7a00aa6de1ad47e04bc1e4d49a9c01
matthew00mckee <67074056+matthew00mckee@users.noreply.github.com>
2021-06-06 23:42:58 -0400

    comment and VCS

--- src/main/java/Exercise_12/simpleInterest.java
+++ src/main/java/Exercise_12/simpleInterest.java
@@ -21,9 +21,11 @@ public class simpleInterest {
         System.out.print("Enter the number of years:");
         String yearsStr = input.next();
         double years = Double.parseDouble(yearsStr);
-
+        //calculates the interest and rounds to the correct number of decimals
         double newAmount = Math.round((principle*(1+rate*years))*100.0)/100.0;
-
+        //this checks to see if we rounded to the correct penny amount
+        // and if the actual interest is higher than the given $ amount it
+        //adds a penny
         if (principle*(1+rate*years)>newAmount){
             newAmount=newAmount+0.01;
         }
